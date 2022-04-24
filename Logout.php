579a0de80ef5ff4cb7080ca0f3df19a5a4ebc2e3
AmirDotNetdev <101495208+AmirDotNetdev@users.noreply.github.com>
2022-04-24 10:58:52 +0430

    Add files via upload

---
+++ Logout.php
@@ -0,0 +1,8 @@
+<?php
+include ("Class/Security.php");
+include ("setting/setting.php");
+include ("lib/db.php");
+    Authentication :: logout();
+    // redirect login
+    // include 'view/logout.php';
+?>
